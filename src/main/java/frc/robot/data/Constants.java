1b4a5ac4345d317fe06e0b15607f8593e813934c
Kabinet <m3kabin@gmail.com>
2026-02-08 18:26:11 -0500

    Update indexer speed

--- src/main/java/frc/robot/data/Constants.java
+++ src/main/java/frc/robot/data/Constants.java
@@ -255,7 +255,7 @@ public final class Constants {
   }
 
   public static class SpindexerConstants {
-    public static final double TEST_VELOCITY = 60.0;
+    public static final double TEST_VELOCITY = 30.0;
 
     public static final double MIN_POSITION_ROTATIONS = Units.degreesToRotations(-360); // Can be up to +/- 360 deg
                                                                                         // without breaking logic
